2d41f04e7b45bc86f3de560dac39a7d9d8884ab0
Timothy Lin <55767165+Deodat-Lawson@users.noreply.github.com>
2025-02-18 22:52:39 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS  # Helps with Cross-Origin Resource Sharing
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS so that your React app can talk to this API
 
 @app.route('/api/profile', methods=['GET'])
 def profile():
