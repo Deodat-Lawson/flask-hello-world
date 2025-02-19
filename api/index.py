4a384e9e73199daaf89e8688f7616044935d6e52
Timothy Lin <55767165+Deodat-Lawson@users.noreply.github.com>
2025-02-18 22:47:18 -0500

    removed app.run

--- api/index.py
+++ api/index.py
@@ -9,6 +9,3 @@ def profile():
         "about": "A full stack developer who loves Python and JavaScript"
     }
     return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5001)
